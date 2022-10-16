9ca4c1d47a7a5793fadffb6a2804ee0adfef58c6
qinxhannah <qin.xia@northeastern.edu>
2022-10-16 16:22:39 -0700

    a5 tuiter

---
+++ public/tuiter/PostSummaryList/posts.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    topic: 'Cooking',
+    userName: 'Eric Kim',
+    time: 'Today',
+    title: 'Brown-Butter Ravioli With Butternut Squash',
+    image: '../images/brown-butter-ravioli.jpg',
+    tweets: '804',
+  },
+  {
+    topic: 'Art & Design',
+    userName: 'Adam Nagourney',
+    time: '1d ago',
+    title: 'Joan Didion and the Western Spirit',
+    image: '../images/joan-didion.jpeg',
+    tweets: '1.3K',
+  },
+  {
+    topic: 'Technology',
+    userName: '',
+    title: 'A New Refrain From Artists: We ‘Almost Gave Up on Instagram',
+    time: '',
+    image: '../images/artist-at-work.jpg',
+    tweets: '55K',
+  },
+  {
+    topic: 'travel',
+    userName: 'Nora Walsh',
+    title: '',
+    time: '15h ago',
+    image: '../images//wfh.png',
+    tweets: '',
+  },];
---
+++ public/tuiter/WhoToFollowList/who.js
@@ -0,0 +1,8 @@
+export default [
+  {   avatarIcon: '../images/coldplay.jpg',
+    userName: 'Coldplay', handle: 'cold_play', },
+  {   avatarIcon: '../images/rihanna.jpg',
+    userName: 'Rihanna', handle: 'rihanna', },
+  {   avatarIcon: '../images/joma-tech.jpg',
+    userName: 'Joma Tech', handle: 'joma_tech', },
+];
